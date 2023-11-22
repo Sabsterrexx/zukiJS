b967f5ed35090448eda95248a3b500b2339f6174
Saabit Husain Zubairi <saabit.zubairi@mail.utoronto.ca>
2023-11-22 15:37:04 -0500

    Added more examples in main.js

--- main.js
+++ main.js
@@ -18,17 +18,20 @@ const API_BACKUP_KEY = ""; //Set this value to your backup API key, if you have 
 
 //The ZukiCall class handles sending and receiving messages to our LLM with the API.
 
-let zukiLLM = new ZukiCall(API_KEY); 
+let zukiAI = new ZukiCall(API_KEY); 
 //By default the chat model is gpt-3.5 if the second function parameter is not defined upon intialization.
 //By default the system prompt (third function parameter) is "You are a helpful assistant", this can be changed later with .zukiChat.setSystemPrompt().
 
-let res = await zukiLLM.zukiChat.sendBackupMessage("Sabs", "Hey, how's it going?"); 
+let chatResponse = await zukiAI.zukiChat.sendMessage("Sabs", "Hey, how's it going?"); 
 /**
  * To call the unfiltered endpoint, use .zukiChat.sendUnfilteredMessage().
  * If you have a backup API endpoint, first set it as a backup using .zukiChat.changeBackupEndpoint(),
  *  then call .zukiChat.sendBackupMessage().
  */
 
-console.log(res); //Response will be printed in the console.
+console.log("Chat Response: ", chatResponse); //Response will be printed in the console.
 //To change the temperature of the model, call .zukiChat.setTemp(). The value by default is 0.7.
-//WARNING: DO NOT USE .zukiImage. It is not stable yet.
+
+//Generate a image with the prompt "A grey tabby cat". The response is returned as a url to the image.
+let imageUrl = await zukiAI.zukiImage.generateImage("A grey tabby cat.");
+console.log("Image Url: ", imageUrl);
